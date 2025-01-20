eff25822b9dc1285d67c0c7691871c6963bcacdd
Alexandros Xiarchos <alex.xiarxos@gmail.com>
2025-01-20 03:09:11 +0200

    update

--- js/home.js
+++ js/home.js
@@ -2,4 +2,19 @@ window.onload = () => {
     setTimeout(() => {
         document.getElementById('home').classList.add('fade-in-active');
     }, 0);
+
+    document.getElementById('projects-button').addEventListener('click', function() {
+        const mainContent = document.getElementById('main-content-md');
+        const projectsSection = document.getElementById('projects-section');
+        const isHeader = mainContent.classList.contains('header');
+
+        if (isHeader) {
+            mainContent.classList.remove('header');
+            projectsSection.classList.add('hidden');
+        } else {
+            mainContent.classList.add('header');
+            projectsSection.classList.remove('hidden');
+        }
+    });
 }
+
