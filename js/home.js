00bf2c887b470962d5179fc7c36aab832cf4dddf
Alexandros Xiarchos <alex.xiarxos@gmail.com>
2025-01-21 22:37:30 +0200

    update

--- js/home.js
+++ js/home.js
@@ -3,6 +3,8 @@ window.onload = () => {
         document.getElementById('home').classList.add('fade-in-active');
     }, 0);
 
+    const footer = document.getElementById('footer');
+
     document.getElementById('projects-button').addEventListener('click', function() {
         const mainContent = document.getElementById('main-content-md');
         const projectsSection = document.getElementById('projects-section');
@@ -11,10 +13,12 @@ window.onload = () => {
         if (!projectsSection.classList.contains('hidden')) {
             projectsSection.classList.add('hidden');
             mainContent.classList.remove('header');
+            footer.classList.add('hidden');
         } else {
             mainContent.classList.add('header');
             projectsSection.classList.remove('hidden');
             experienceSection.classList.add('hidden');
+            footer.classList.remove('hidden');
         }
     });
 
@@ -26,10 +30,12 @@ window.onload = () => {
         if (!experienceSection.classList.contains('hidden')) {
             experienceSection.classList.add('hidden');
             mainContent.classList.remove('header');
+            footer.classList.add('hidden');
         } else {
             mainContent.classList.add('header');
             experienceSection.classList.remove('hidden');
             projectsSection.classList.add('hidden');
+            footer.classList.remove('hidden');
         }
     });
 }
