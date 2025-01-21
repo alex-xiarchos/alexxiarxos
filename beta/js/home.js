7f7e882eafa347dce704d923066ca4e10ac05326
Alexandros Xiarchos <alex.xiarxos@gmail.com>
2025-01-21 22:37:30 +0200

    update

---
+++ beta/js/home.js
@@ -0,0 +1,41 @@
+window.onload = () => {
+    setTimeout(() => {
+        document.getElementById('home').classList.add('fade-in-active');
+    }, 0);
+
+    const footer = document.getElementById('footer');
+
+    document.getElementById('projects-button').addEventListener('click', function() {
+        const mainContent = document.getElementById('main-content-md');
+        const projectsSection = document.getElementById('projects-section');
+        const experienceSection = document.getElementById('experience-section');
+
+        if (!projectsSection.classList.contains('hidden')) {
+            projectsSection.classList.add('hidden');
+            mainContent.classList.remove('header');
+            footer.classList.add('hidden');
+        } else {
+            mainContent.classList.add('header');
+            projectsSection.classList.remove('hidden');
+            experienceSection.classList.add('hidden');
+            footer.classList.remove('hidden');
+        }
+    });
+
+    document.getElementById('experience-button').addEventListener('click', function() {
+        const mainContent = document.getElementById('main-content-md');
+        const projectsSection = document.getElementById('projects-section');
+        const experienceSection = document.getElementById('experience-section');
+
+        if (!experienceSection.classList.contains('hidden')) {
+            experienceSection.classList.add('hidden');
+            mainContent.classList.remove('header');
+            footer.classList.add('hidden');
+        } else {
+            mainContent.classList.add('header');
+            experienceSection.classList.remove('hidden');
+            projectsSection.classList.add('hidden');
+            footer.classList.remove('hidden');
+        }
+    });
+}
